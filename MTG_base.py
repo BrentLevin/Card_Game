31fb4f5574bf3a2abb2d07fc6d5263de28cc956e
BroTobro <brentonlevin@gmail.com>
2024-01-14 20:48:05 +1100

    added deck building

--- MTG_base.py
+++ MTG_base.py
@@ -1,4 +1,4 @@
-class Card:
+class Creature():
     def __init__(self, name, cardtype, faction, power, toughness, colourless = 0, coloured = [], flash: bool = False, flying: bool = False):
         self.name = name
         self.colourless = colourless
@@ -9,8 +9,33 @@ class Card:
         self.toughness = toughness
         self.flash = flash
         self.flying = flying
+    
+class Deck():
+    def __init__(self):
+        self.cards = []
 
-RestorationAngel = Card(name = "Restoration Angel"
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def build_deck(self, card_list):
+        for i in card_list:
+            self.add_card(i)
+
+    #need something to import a deck list
+        
+
+restoration_angel = Creature(name = "Restoration Angel"
+                        , cardtype = "Creature"
+                        , colourless = 3
+                        , coloured = ["w"]
+                        , faction = "Angel"
+                        , power = 3
+                        , toughness = 4
+                        , flash = True
+                        , flying = True
+                        )
+
+high_sentinels_of_arashin = Creature(name = "High Sentinels of Arashin"
                         , cardtype = "Creature"
                         , colourless = 3
                         , coloured = ["w"]
@@ -22,8 +47,22 @@ RestorationAngel = Card(name = "Restoration Angel"
                         )
 
 
+my_deck = Deck()
+
+deck_list = [high_sentinels_of_arashin,restoration_angel]
+
+my_deck.build_deck(deck_list)
+
+# for i in deck_list:
+#     my_deck.add_card(i)
+
 def main():
-    print(vars(RestorationAngel))
+    # print(vars(RestorationAngel))
+    # print(str(my_deck.cards()))
+    for obj in my_deck.cards:
+        print(obj.name)
+
 
 if __name__ == "__main__":
     main()
+
