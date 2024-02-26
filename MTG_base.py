180a68971572f75a42b4ca360a804739bece94d4
BroTobro <brentonlevin@gmail.com>
2024-02-26 15:22:39 +1100

    changes

--- MTG_base.py
+++ MTG_base.py
@@ -93,9 +93,10 @@ class Deck:
         self.active_player.total_player_battlefield.append(self.stack[-1]) #assumig it resolves to battle field sorcery resolves will be to different location
         del self.stack[-1]
         
+
     def play_card(self, start_player_index): #need constraints to maintain cards being played can actually be played whether it be at instant or sorcery speed
         finality_of_priority = 0
-
+    
         while True:
             print(len(self.stack))
             card_to_play = None
